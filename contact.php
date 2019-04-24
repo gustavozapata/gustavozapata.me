231af035ce0581495f671e2fa968a30b9b818276
Gustavo Zapata <tavordie@hotmail.com>
2019-04-25 00:52:04 +0100

    improving contact form

--- contact.php
+++ contact.php
@@ -2,19 +2,19 @@
 
 if(isset($_REQUEST["message"])){
   $name = $email = $message = "";
-  $name = curarInput($_POST['name']);
-  $email = curarInput($_POST['email']);
-  $message = curarInput($_POST['message']);
+  $name = sanitiseInput($_POST['name']);
+  $email = sanitiseInput($_POST['email']);
+  $message = sanitiseInput($_POST['message']);
 
   $formcontent=" From: $name \n Message: $message";
   $recipient = "contact@gustavozapata.me";
   $subject = "GZ Contact Form";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $formcontent, $email) or die("Error!");
-  header("Location: index.html");
+  header("Location: index.html?contact=1");
 }
 
-function curarInput($elInput){
+function sanitiseInput($elInput){
   $elInput = trim($elInput);
   $elInput = stripslashes($elInput);
   $elInput = htmlspecialchars($elInput);
